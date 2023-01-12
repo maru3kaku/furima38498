ea1468ae1e3f6adb53c3e44de0a18f1fd09256eb
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-12 12:00:58 +0900

    userテーブルバリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller? #デバイスコントローラーの処理がtrueの場合
 
   private
 
@@ -8,5 +8,19 @@ class ApplicationController < ActionController::Base
     authenticate_or_request_with_http_basic do |username, password|
       username == 'qwerty' && password == '2222'
     end
+
+    def configure_permitted_parameters  # メソッド名は慣習
+      # deviseのUserモデルにパラメーターを許可
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:許可するキー])
+    end
+
+
+
+
+
+
+
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :birthday, presence: true
+  validates :name_kana, presence: true
+  validates :name, presence: true
+  validates :sei_kana, presence: true
+  validates :sei , presence: true
+  validates :nickname, presence: true
+  
 end
