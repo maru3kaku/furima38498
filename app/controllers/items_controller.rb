3b3d77600dbff9a4fac9f15a93289c66696725bb
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-12 21:01:43 +0900

    ログアウト機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,13 @@ class ItemsController < ApplicationController
   def create
   end
 
+  def destroy
+    if item = Item.find(params[:id])
+      item.destroy
+     redirect_to root_path
+   else
+     redirect_to prototype_path
+   end
+ end
+
 end
