b5744aac05b4d8ad0fa1b3186aa3df4c02ed934a
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-13 17:03:21 +0900

    エラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
         
   validates :email, presence: true
-  validates :password, presence: true
+  validates :password, presence: true 
   validates :birthday, presence: true
   validates :name_kana, presence: true
   validates :name, presence: true
