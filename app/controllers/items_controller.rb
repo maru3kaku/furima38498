3d424a13da3d0ec6856ec17b7a7e073717c65834
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-18 13:41:06 +0900

    バリデーション・テストコードの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,3 @@ class ItemsController < ApplicationController
     
   end
   
-  
- private
-
- def item_params
-   params.require(:item).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
- end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,22 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
+        @user.password = @user.password_confirmation
+        @user.nickname = @user.nickname
+        @user.email = @user.email
+        @user.valid?
+      end
+    end
+
+    context '新規登録できないとき' do
     it "メールアドレスが一意性であること。" do
       @user.save
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include('Email has already been taken')
+      expect(another_user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
       end
     it "メールアドレスが必須であること。" do
       @user.email = ''
@@ -28,6 +38,21 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password can't be blank")
     end
+    it "パスワードは英字のみでは登録できない" do
+      @user.password = 'qwerty'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+    end
+    it "パスワードは数字のみでは登録できない" do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+    end
+    it "パスワードは全角含むと登録できない" do
+      @user.password = 'あかさたなは'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+    end
     it "パスワードは、6文字以上での入力が必須であること。" do
       @user.password = '12345'
       @user.password_confirmation = '12345'
@@ -43,12 +68,39 @@ RSpec.describe User, type: :model do
     it '名前にひらがな漢字かなのみの入力が必須であること' do
       @user.name = 'asdf'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Name is invalid. Input full-width characters.")
-end  
+      expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+    end
 it '姓にひらがな漢字かなのみの入力が必須であること' do
   @user.sei = 'asdf'
   @user.valid?
-  expect(@user.errors.full_messages).to include("Sei is invalid. Input full-width characters.")
-end  
-  end
-  end
+  expect(@user.errors.full_messages).to include("Sei is invalid. Input full-width characters.","Name kana is invalid. Input full-width characters.","Sei kana is invalid. Input full-width characters.","Birthday is invalid. Input full-width characters.")
+end
+it "名前が必須であること" do
+  @user.name = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name can't be blank")
+end
+it "姓が必須であること" do
+  @user.sei = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Sei can't be blank")
+end
+it "名前(カナ)が必須であること" do
+  @user.name_kana = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+end
+it "姓(カナ)が必須であること" do
+  @user.sei_kana = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+end
+it "誕生日が必須であること" do
+  @user.birthday = ''
+  @user.valid?
+  expect(@user.errors.full_messages).to include("Name kana is invalid. Input full-width characters.", "Sei kana is invalid. Input full-width characters.", "Birthday is invalid. Input full-width characters.")
+end
+
+end
+end
+end
