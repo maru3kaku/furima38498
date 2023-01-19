6818d58884e75d0549f47804eced6945bb85ee3c
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-19 16:33:25 +0900

    itemテーブルのバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   end
   
   def new
-    @item = item.new
+    
   end
   
   def message_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  validate :title, presence: true                       #商品名
+  validates :concept, presence: true                    #商品の説明
+  validates :category_id  ,presence: true              #カテゴリー
+  validates :status_id, presence: true                  #商品状態
+  validates :delivery_charge_burden_id , presence: true #配送料の負担
+  validates :prefecture_id,presence: true              #発送元の地域
+  validates :shipping_day_id, presence: true            #発送までの日数
+  validates :price, presence: true  
+
+
   belongs_to :user
   has_one_attached :image
 end
