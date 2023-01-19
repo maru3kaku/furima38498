ee56f42ad0e6d59d5e1cf9f87558474d8eea5f6d
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-19 11:52:43 +0900

    Articleモデルのアソシエーション

--- db/migrate/20230119024116_create_articles.rb
+++ db/migrate/20230119024116_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       
-      t.integer    :article_id     , null: false
+      t.integer    :genre_id     , null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2023_01_19_024116) do
   end
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "article_id", null: false
+    t.integer "genre_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
