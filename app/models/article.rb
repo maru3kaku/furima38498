f79fae94ffaf8e0cfa18372256478210790912ba
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-19 14:15:02 +0900

    各アクティブハッシュモデル作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,9 @@
 class Article < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
+  belongs_to :Category
+  belongs_to :status
+  validates :title, :text, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
   devise_for :users
 root to: 'items#index'
 resources :items, only: [:index,:new] do
 end
-
+resources :articles
 end
