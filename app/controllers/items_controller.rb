4ccb4d5c5636b62a7b4841cacc3aa4dc9db4e597
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-20 09:03:36 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,17 @@ class ItemsController < ApplicationController
   def new
   
   end
+
+
+  def create
+      @item = Item.new(prototype_params)    
+      if @Item.save
+        redirect_to root_path
+      else 
+        render :new
+    end
+  end
+  end
   
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
---
+++ app/javascript/item_price.js
@@ -0,0 +1,7 @@
+// 金額を入力した数値をpriceInputという変数に格納する
+const priceInput = document.getElementById("item_price");
+priceInput.addEventListener("input", () => {
+  console.log("イベント発火");
+
+
+})
