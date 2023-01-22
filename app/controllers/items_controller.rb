3fb28d8b2b7cb93b50be3dfb86a39facc58f454f
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-22 18:07:32 +0900

    出品ページのエラーハンドリングの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,15 +10,16 @@ class ItemsController < ApplicationController
 
   def create
       @item = Item.new(item_params)    
-      if @Item.save
+      if @item.save
         redirect_to root_path
       else 
         render :new
     end
   end
-  end
+  
   
   def item_params
     params.require(:item).permit(:image,:title,:concept,:status_id,:category_id,:delivery_charge_burden_id,:prefecture_id,:shipping_day_id,:price).merge(user_id: current_user.id)
   end
 
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,11 @@
 class Item < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :validatable
-
-  validate :image, presence: true
-  validate :title, presence: true, length: { maximum: 40 }                         #商品名
+  validates :image, presence: true
   validates :concept, presence: true, length: { maximum: 1000 }                   #商品の説明
-  validates :category_id  ,presence: true              #カテゴリー
-  validates :status_id, presence: true                  #商品状態
-  validates :delivery_charge_burden_id , presence: true #配送料の負担
-  validates :prefecture_id,presence: true              #発送元の地域
-  validates :shipping_day_id, presence: true            #発送までの日数
+   validates :category_id  ,presence: true              #カテゴリー
+   validates :status_id, presence: true                  #商品状態
+   validates :delivery_charge_burden_id , presence: true #配送料の負担
+   validates :prefecture_id,presence: true              #発送元の地域
+   validates :shipping_day_id, presence: true            #発送までの日数
   validates :price, presence: true,numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
 
   belongs_to :user
@@ -21,9 +17,6 @@ class Item < ApplicationRecord
   validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
 
-  validates :concept,
-  validates :price, format:{with: /\A[0-9]+\z/i, message: "is invalid. Input Half-width characters."},
+  validates :price, format:{with: /\A[0-9]+\z/i, message: "is invalid. Input Half-width characters."}
   
-end
-end
 end
