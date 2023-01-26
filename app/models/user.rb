5d374c42c2e22800dc04ebf053180c8eb31f66b5
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-26 20:35:15 +0900

    rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-validates :name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-validates :sei, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-validates :name_kana, format:{with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width characters."}
-validates :sei_kana, format:{with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width characters."}
-validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input full-width characters."}
+  validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :sei, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :sei_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width characters.' }
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Input full-width characters.' }
 
   validates :birthday, presence: true
   validates :name_kana, presence: true
   validates :name, presence: true
   validates :sei_kana, presence: true
-  validates :sei , presence: true
+  validates :sei, presence: true
   validates :nickname, presence: true
- 
+
   has_many :items
 end
