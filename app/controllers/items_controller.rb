8ccd4742676f1ee5c0cf68898318961549c9895b
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-01-29 16:55:09 +0900

    コントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,8 +19,21 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+   if @item.update(item_params)
+     redirect_to item_path
+   else
+     render :edit
+   end
+ end
+
 
   def item_params
     params.require(:item).permit(:image, :title, :concept, :status_id, :category_id, :delivery_charge_burden_id, :prefecture_id,
