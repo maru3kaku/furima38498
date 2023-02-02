708beca214a3b86b99fec52d9f76946f3836fadc
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-02-02 23:17:18 +0900

    各バリデーションの詳細の追加

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,9 +1,12 @@
 class Delivery < ApplicationRecord
-  validates :post_code, presence: true
-  validates :prefecture_id, presence: true
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  validates :prefecture_id, presence: true{ other_than: 0, message: "can't be blank" }
   validates :city, presence: true
   validates :address, presence: true
-  validates :telephone_number, presence: true
-  
+  validates :telephone_number, presence: true,format: {with: /\A[0-9]\z/,message: "can't be blank"}
 
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
