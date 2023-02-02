d59fd30645351aef3c83a6b4f5c718ee4abb6945
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-02-02 22:57:29 +0900

    オーダー画面の編集

--- db/migrate/20230202112145_create_orders.rb
+++ db/migrate/20230202112145_create_orders.rb
@@ -3,9 +3,10 @@ class CreateOrders < ActiveRecord::Migration[6.0]
     create_table :orders do |t|
       t.integer :price ,null: false
       t.integer :item ,null: false
-      t.integer :card_year ,null: false
-      t.integer :card_month ,null: false
-      t.integer :card_cvc ,null: false
+      t.integer :card_no ,null: false         #カードの番号
+      t.integer :card_year ,null: false #カードの有効期限(年)
+      t.integer :card_month ,null: false #カードの有効期限（月） 
+      t.integer :card_cvc ,null: false        #カードのCVC
       t.timestamps
     end
   end
