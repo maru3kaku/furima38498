ccc9ad45be4e9244ec85db1afa4eb3a165556175
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-02-02 22:59:42 +0900

    orderのバリデーションの設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   validates :price, presence: true
-  validates :price, presence: true
-  validates :price, presence: true
-  validates :price, presence: true
+  validates :card_no , presence: true
+  validates :card_year , presence: true
+  validates :card_month, presence: true
+  validates :card_cvc, presence: true
 end
