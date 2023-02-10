61ae0bc4bc5fd3cf331155ac71d5020c9f8bbe51
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-02-10 11:39:45 +0900

    order_factorybotの作成

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,9 +1,17 @@
 FactoryBot.define do
   factory :order do
-    price {3000}
     title { 'オム' } # 商品名
-    price { 10000 } # 価格
-    delivery_charge_burden_id { 2 } # 配送料の負担
+    price { '10000' } # 価格
+    post_code{'123-4567'}#郵便番号
+    prefecture_id {'30'}#都道府県
+    city {'横浜市緑区'}#市区町村
+    address {'1-1-1'}#番地
+    building_name {'やなぎビル'}#建物名(任意)
+    telephone_number{'09012345678'}#電話番号
+
+
+    
+    
     #{4 * 12 } #カード番号
     #{ 25 } #カード有効期限（年）
     #{ 12 } #カード有効期限（月）
