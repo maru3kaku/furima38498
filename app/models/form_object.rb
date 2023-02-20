445be218fe6a8d8265f3c7248ac1bb492c89deb8
maru3kaku <115141933+maru3kaku@users.noreply.github.com>
2023-02-20 10:50:40 +0900

    モデルの修正

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -4,9 +4,7 @@ class FormObject
                 :price, :token
 with_options presence: true do
   validates :token
-  validates :post_code
-  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid.' }
-  validates :prefecture_id
+  validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/}
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :city
   validates :address
